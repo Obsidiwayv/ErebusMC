7d627391b77bd61859e9f980b669c0c3f3a611e0
Obsidiwayv <pizza5803@gmail.com>
2023-06-01 12:50:28 -0400

    Add project files.

---
+++ ErebusLauncher/Java.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErebusLauncher
+{
+    internal class Java
+    {
+        private static RegistryKey? key;
+
+        public static void OpenRegistry()
+        {
+            key = Registry.LocalMachine.OpenSubKey(name: "SOFTWARE\\JavaSoft\\JDK");
+        }
+
+        public static String getVersion()
+        {
+            if (key == null)
+            {
+                return "Java not installed";
+            } else
+            {
+
+            }
+        }
+    }
+}
